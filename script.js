bc51f171245f842db250fc3aa31c48550ae66943
Andre Pacola <andreluizzpacolla@outlook.com>
2023-08-16 21:35:03 -0300

    adc melhorias no respondive

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('.menu .mobile').addEventListener('click',()=>{
+   if(document.querySelector('nav').style.display == 'flex'){
+      document.querySelector('nav').style.display = 'none'
+   }else{
+      document.querySelector("nav").style.display = 'flex';
+   }
+})
